b1e1c4d7b97aadcb99a8c01ba4be1011fcca922c
MKozai <73626262+MKozai@users.noreply.github.com>
2025-10-20 16:05:11 +0900

    Modifications for cases without default namespace prefix and with unused element names

--- xml_to_excel.py
+++ xml_to_excel.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 import sys
 import fire
 import pandas as pd
@@ -29,8 +30,7 @@ def xml_to_excel(elem_table: str, xmldir: str, outdir: str='./'):
     if not os.path.exists(outdir):
         os.mkdir(outdir)
 
-    list_tmp = [os.path.join(xmldir, s) for s in os.listdir(xmldir)]
-    xmlfiles = sorted([s for s in list_tmp if os.path.isfile(s) ])
+    xmlfiles = [str(p) for p in Path(xmldir).rglob('*.xml')]
     print('num of xmlfiles: ' + str(len(xmlfiles)))
 
     print('')
@@ -53,22 +53,29 @@ def xml_to_excel(elem_table: str, xmldir: str, outdir: str='./'):
     print('Compile XML values')
 
     n = 0
+    pref_default = '_ns'
     for xmlfile in xmlfiles:
         xmlfile0 = xmlfile.split('/')[-1]
         print(xmlfile0, end=' ', flush=True)
 
         xml = etree.parse(xmlfile)
         root = xml.getroot()
-        ns = root.nsmap
+        ns_tmp = root.nsmap
+        ns = {}
+        for key_tmp in ns_tmp:
+            key = pref_default if not key_tmp else key_tmp
+            ns[key] = ns_tmp[key_tmp]
 
-        table_xml = [l for l in module.read_xml(dict_path, root, ns, 'XPath')]
+        table_xml = [l for l in module.read_xml(dict_path, root, ns, pref_default, 'XPath')]
 
         table_name = [l[0] for l in table_xml]
         data_xml = pd.DataFrame(table_name)
+        for col in cols_name:
+            if col not in data_xml.columns:
+                data_xml[col] = None
         data_xml = module.data_to_str(data_xml)
 
         data_xml[xmlfile0] = [l[1] for l in table_xml]
-
         if n>0:
             data_merge = pd.merge(data_merge, data_xml, on = cols_name, how = 'outer')
         else:
@@ -92,14 +99,27 @@ def xml_to_excel(elem_table: str, xmldir: str, outdir: str='./'):
     dict_flatten = flatten(dict_data_sort)
 
     table_name = [[s for s in t] for t in dict_flatten.keys()]
+    data_name = pd.DataFrame(table_name)
+    for col in cols_name:
+        if col not in data_name.columns:
+            data_name[col] = None
+
     list_val = [l for l in dict_flatten.values()]
-    data = pd.concat([pd.DataFrame(table_name), pd.DataFrame(list_val)], axis=1)
+    data = pd.concat([data_name, pd.DataFrame(list_val)], axis=1)
     data.columns = data_merge.columns
     data = module.data_to_str(data)
+    cols_name_new = []
+    for col in cols_name:
+        if (data[col]=='').all():
+            data = data.drop(columns=col)
+            continue
+        cols_name_new.append(col)
 
+    print('')
+    print('Output')
     outfile = outdir + '/table.pkl'
     module.pickle_dump(data, outfile)
-    print('output: ' + outfile)
+    print(outfile)
 
     outfile = outdir + '/table.xlsx'
     if os.path.exists(outfile):
@@ -109,15 +129,14 @@ def xml_to_excel(elem_table: str, xmldir: str, outdir: str='./'):
         data.to_excel(writer, sheet_name='Sheet1', index=False)
         ws = writer.sheets['Sheet1']
 
-        print('')
         print('Merge Excel cells')
         start_row = 2
         end_row = start_row + len(data) - 1
         dict_merge_v = {}
-        for col in range(1, len(cols_name) + 1):
+        for col in range(1, len(cols_name_new) + 1):
             dict_merge_v[col] = module.list_merge_vertical(ws, col, start_row, end_row)
 
-        dict_merge_h = module.dict_merge_horizontal(ws, start_row, end_row, 1, len(cols_name))
+        dict_merge_h = module.dict_merge_horizontal(ws, start_row, end_row, 1, len(cols_name_new))
 
         for col, list_merge in dict_merge_v.items():
             for lim in list_merge:
@@ -129,7 +148,7 @@ def xml_to_excel(elem_table: str, xmldir: str, outdir: str='./'):
     
         print('Remove temporary suffix in element names')
         for row in range(start_row, end_row + 1):
-            for col in range(1, len(cols_name) + 1):
+            for col in range(1, len(cols_name_new) + 1):
                 cell = ws.cell(row = row, column = col)
                 cell.alignment = Alignment(horizontal = 'left', vertical = 'center')
                 if isinstance(cell.value, str):
